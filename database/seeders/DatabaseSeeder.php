aa432b96d05add19ea4f8c9668a5dd35fb0d30b8
RobertArsyah <fahriarsyah07@gmail.com>
2026-02-24 11:50:48 +0700

    DELETE | Comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'superadmin@simedik.com'], // Cek berdasarkan email
+            ['email' => 'superadmin@simedik.com'],
             [
                 'name' => 'Super Admin',
                 'email_verified_at' => now(),
